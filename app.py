5fed85af7f6d3ba895564ba71449f3cde1b77d59
Vyxr890 <pandevyankatesh13@gmail.com>
2025-12-04 13:21:11 -0500

    Frontend 1st

---
+++ app.py
@@ -0,0 +1,31 @@
+##Hhello all
+umr guzregī imtihān meñ kyā
+daaġh hī deñge mujh ko daan meñ kyā
+
+merī har baat be-asar hī rahī
+nuqs hai kuchh mire bayān meñ kyā
+
+mujh ko to koī Toktā bhī nahīñ
+yahī hotā hai ḳhāndān meñ kyā
+
+apnī mahrūmiyāñ chhupāte haiñ
+ham ġharīboñ kī ān-bān meñ kyā
+
+ḳhud ko jaanā judā zamāne se
+aa gayā thā mire gumān meñ kyā
+
+shaam hī se dukān-e-dīd hai band
+nahīñ nuqsān tak dukān meñ kyā
+
+ai mire sub.h-o-shām-e-dil kī shafaq
+tū nahātī hai ab bhī baan meñ kyā
+
+bolte kyuuñ nahīñ mire haq meñ
+aable paḌ ga.e zabān meñ kyā
+
+
+print{kitne jamlim hai wo log jo bolte hai 
+tod do phool phool chodho mat 
+baugbaan mera yeh kehna hai 
+dekhlo phool phool todo mat}
+##hello all
